a956f8b1f1a5b827871b68a6b0048768ab9c027b
peedrosh <developer.peedrojs@gmail.com>
2021-02-23 11:21:32 -0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/peedrosh.png" alt="Pedro Cardoso" />
+
+      <div>
+        <strong>Pedro Cardoso</strong>
+        <p><img src="icons/level.svg" alt="Level" /> Level 1</p>
+      </div>
+    </div>
+  )
+}
